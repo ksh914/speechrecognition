ee0a67f4fc979631d20e82da4e95611ea8e2ac2a
ksh914 <87693086+ksh914@users.noreply.github.com>
2021-11-30 02:46:52 +0900

    html plus

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        print("FORM DATA RECEIVED")
+
+        if "file" not in request.files:
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5001', debug=True)
