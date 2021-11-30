4fd09d6ea8504521c27903afcf623b1f5a0aef15
ksh914 <87693086+ksh914@users.noreply.github.com>
2021-11-30 17:32:00 +0900

    adding transcription and recognize_google

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+import speech_recognition as sr
+
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    transcript=""
     if request.method == "POST":
         print("FORM DATA RECEIVED")
 
         if "file" not in request.files:
-    return render_template('index.html')
+            return redirect(request.url)
+
+        file = request.files["file"]
+        if file.filename == "":
+            return redirect(request.url)
+
+        if file:
+            recognizer = sr.Recognizer()
+            audioFile = sr.AudioFile(file)
+            with audioFile as source:
+                data = recognizer.record(source)
+            transcript = recognizer.recognize_google(data, key=None, language='ko')
+
+    return render_template('index.html', transcript=transcript)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5001', debug=True)
+    app.run(host='192.168.0.5', port='5001', debug=True)
