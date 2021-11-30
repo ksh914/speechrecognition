45476106ecf9277c1383ec47d088c646081a49db
ksh914 <87693086+ksh914@users.noreply.github.com>
2021-11-30 19:58:26 +0900

    success

--- app.py
+++ app.py
@@ -6,27 +6,21 @@ app = Flask(__name__)
 
 
 @app.route("/", methods=["GET", "POST"])
-def index():
+def index3():
     transcript=""
-    if request.method == "POST":
-        print("FORM DATA RECEIVED")
-
-        if "file" not in request.files:
-            return redirect(request.url)
-
-        file = request.files["file"]
-        if file.filename == "":
-            return redirect(request.url)
-
-        if file:
-            recognizer = sr.Recognizer()
-            audioFile = sr.AudioFile(file)
-            with audioFile as source:
-                data = recognizer.record(source)
-            transcript = recognizer.recognize_google(data, key=None, language='ko')
-
-    return render_template('index.html', transcript=transcript)
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        print("말씀하세요")
+        audio = r.listen(source)
+    try:
+        transcript = r.recognize_google(audio, language="ko-KR")
+        print(transcript)
+    except sr.UnknownValueError:
+        print("다시 말씀해 주세요")
+    except sr.RequestError as e:
+        print("결과값을 불러오지 못했습니다.; {0}".format(e))
+    return render_template('index3.html', transcript=transcript)
 
 
 if __name__ == "__main__":
-    app.run(host='192.168.0.5', port='5001', debug=True)
+    app.run('0.0.0.0', port=5001, debug=True)
