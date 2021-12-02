9a5685a8fc9ef04c8399d1aaf4e8839bcf100fc7
ksh914 <87693086+ksh914@users.noreply.github.com>
2021-12-03 01:41:46 +0900

    STT SUCCESS

--- app.py
+++ app.py
@@ -5,22 +5,10 @@ import speech_recognition as sr
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route('/', methods=["GET", "POST"])
 def index3():
-    transcript=""
-    r=sr.Recognizer()
-    with sr.Microphone() as source:
-        print("말씀하세요")
-        audio = r.listen(source)
-    try:
-        transcript = r.recognize_google(audio, language="ko-KR")
-        print(transcript)
-    except sr.UnknownValueError:
-        print("다시 말씀해 주세요")
-    except sr.RequestError as e:
-        print("결과값을 불러오지 못했습니다.; {0}".format(e))
-    return render_template('index3.html', transcript=transcript)
+    return render_template('speechToText.html')
 
 
 if __name__ == "__main__":
-    app.run('0.0.0.0', port=5001, debug=True)
+    app.run('127.0.0.1', port=5001, debug=True)
